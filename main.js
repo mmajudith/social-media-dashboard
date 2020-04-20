f937f4fcc427dfc7a52c3e1f206a9e90b9fac75a
mma judith <mmajudith2000@gmail.com>
2020-04-20 10:23:40 -0700

    Social Media Dashboard

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+"use strict";
+
 const toggle = document.querySelector('#toggle');
+const toggle2 = document.querySelector('#toggle2');
 const body = document.querySelector('div');
 
+
 toggle.addEventListener('click', () =>{
+    body.classList.toggle('dark');
+    body.classList.toggle('light');
+
+});
+
+toggle2.addEventListener('click', () =>{
     body.classList.toggle('dark');
     body.classList.toggle('light');
 
